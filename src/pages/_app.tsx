9784c10d15107a52b3fde172f9dc49bd6bb9fc25
VadimZaporozhets <zaporozhets.vadim@gmail.com>
2022-09-28 20:29:46 +0300

    add tRPC

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppType } from "next/dist/shared/lib/utils";
+import { trpc } from "@/utils/trpc";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp: AppType = ({ Component, pageProps }) => {
+	return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default trpc.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,28 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import { trpc } from "@/utils/trpc";
 
 const Home: NextPage = () => {
-  return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center">
-      <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
-      <div className="p-2"></div>
-      <div className="border rounded p-8 justify-between items-center max-w-2xl flex">
-        <div className="w-16 h-16 bg-purple-300"></div>
-        <div className="p-8">VS</div>
-        <div className="w-16 h-16 bg-purple-300"></div>
-      </div>
-    </div>
-  )
-}
+	const { data, isLoading } = trpc.hello.useQuery({ text: "Vadim" });
 
-export default Home
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+
+	if (data) {
+		return <div>{data.greeting}</div>;
+	}
+
+	return (
+		<div className="h-screen w-screen flex flex-col items-center justify-center">
+			<div className="text-2xl text-center">Which Pokemon is Rounder?</div>
+			<div className="p-2"></div>
+			<div className="border rounded p-8 justify-between items-center max-w-2xl flex">
+				<div className="w-16 h-16 bg-purple-300"></div>
+				<div className="p-8">VS</div>
+				<div className="w-16 h-16 bg-purple-300"></div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
